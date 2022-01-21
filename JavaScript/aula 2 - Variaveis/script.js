4602d18e069a73563363997c5abc9d07a1e9f34f
Emily Gil <emily.camily.gil.santos@gmail.com>
2022-01-21 00:16:44 -0300

    Adicionadas anotações das aulas 1 e 2 de JavaScript

---
+++ JavaScript/aula 2 - Variaveis/script.js
@@ -0,0 +1,68 @@
+//tipos primitivos de variaveis
+
+//bollean
+/*
+var vDuF = false;
+console.log(typeof(vDuF));
+*/
+
+//number
+/*
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+**/
+
+//string
+/*
+var nome = 'emily';
+console.log(typeof(nome));
+*/
+
+//como declarar
+/*
+var variavel='emily';
+variavel = 'gil';
+console.log(variavel)
+
+let variavel2 = 'emily';
+variavel2 = 'camily';
+console.log(variavel2);
+
+const constante = 'Emily Gil';
+console.log(constante)
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+escopoLocal();
+
+
+//adição
+var adicao = 1+1;
+console.log(adicao);
+
+//subtração
+var subtracao = 3-1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2*3;
+console.log(multiplicacao);
+
+//divisao real
+var divisaoReal = 6/2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5%2;
+console.log(divisaoInteira);
+
+//potenciação
+var pot = 2 ** 10;
+console.log(pot);
+
+*/
