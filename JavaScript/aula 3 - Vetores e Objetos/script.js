076b44ad8de39bf34e5d1e44a62f7c01cfd1a320
Emily Gil <emily.camily.gil.santos@gmail.com>
2022-01-21 10:45:15 -0300

    Adicionadas anotações das aulas 3 e 4

---
+++ JavaScript/aula 3 - Vetores e Objetos/script.js
@@ -0,0 +1,20 @@
+//o que são vetores ou arrays
+
+//como declarar um array
+/*let array = ['string', 1, true];
+console.log(array);*/
+
+//arrays podem guardar vários tipos de dados
+
+//forEach() - itera um array
+//push() - add item no final do array
+//pop() - remove item no final do array
+//shift() - remove item no inicio do array
+//unshift() - add item no inicio do array
+//indexOf() - retorna o indice de um valor
+//splice() - remove ou substitui um item pelo indice
+//slice() - retorna uma parte de um array existente
+
+//objetos
+let object = {string: 'string', number: 1, boolean: true, array: ["array"], objectInterno: {objectInterno:'objeto interno'}};
+console.log(object)
